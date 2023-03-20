a62d67c8b538eef44b77ce2bcb2a8483a916857b
klthemathnerd8 <91514631+klthemathnerd8@users.noreply.github.com>
2023-03-19 20:14:06 -0700

    Add files via upload

---
+++ sites/index.js
@@ -0,0 +1,19 @@
+function loadDisney() {
+  window.location.replace('https://lumanom.v10-13.repl.co/service/hvtrs8%2F-wuw%2Cdksle%7Bpnuq.aoo%2F');
+}
+
+function loadWebtoon() {
+  window.location.replace('')
+}
+
+function load() {
+  window.location.replace('')
+}
+
+function load() {
+  window.location.replace('')
+}
+
+function load() {
+  window.location.replace('')
+}
